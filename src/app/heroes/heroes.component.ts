acf9a0d5bd126026e025e270f157c4a258764b75
jeffrey <jeffrey.nasson@gmail.com>
2019-03-07 22:45:50 -0700

    hero list and onSelect

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes', //components css selector or css file
@@ -8,10 +9,13 @@ import {Hero} from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero ={
-    id:1,
-    name: 'Windstorm'
+  selectedHero: Hero; 
+
+  onSelect(hero: Hero): void{ //binds to the click event in the ./heroes.component.html file
+    this.selectedHero = hero
   }
+
+  heroes = HEROES
 
   constructor() { }
 
