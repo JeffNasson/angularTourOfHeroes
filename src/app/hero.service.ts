19c49181d3523e51628accaf4795105617b2284a
jeffrey <jeffrey.nasson@gmail.com>
2019-03-08 01:16:56 -0700

    message service setup

---
+++ src/app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+import {MessageService} from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]>{
+    // sends the message after fetching heroes
+    this.messageService.add('HeroService: fetched heroes')
+    return of(HEROES)
+  }
+  // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+
+  constructor(private messageService: MessageService) { }
+}
+
+// Notice that the new service imports the Angular Injectable symbol and annotates the class with the @Injectable() decorator. This marks the class as one that participates in the dependency injection system. 
+
+//The @Injectable() decorator accepts a metadata object for the service, the same way the @Component() decorator did for your component classes.
