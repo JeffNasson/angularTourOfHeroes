5086757353e929075c38f3b39fce82ad8db1b576
jeffrey <jeffrey.nasson@gmail.com>
2019-03-13 12:02:05 -0600

    Routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import {HeroesComponent} from './heroes/heroes.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 
 //Begin Routes
 
 const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent}
+  {path:'', redirectTo: '/dashboard', pathMatch: 'full'}, //redirects empty paths to /dashboard
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HeroDetailComponent},
+  {path: 'heroes', component: HeroesComponent},
 ];
 
 //End Routes
